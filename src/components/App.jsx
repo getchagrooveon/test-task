6176dd75e3c7cefd6deefd1c1036a66b3c96ae7e
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-07-02 08:36:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import {useEffect} from 'react';
 import UserCard from './UserCard/UserCard';
-import {getUsers} from '../services/API';
+// import {getUsers} from '../services/API';
 import {useDispatch} from 'react-redux';
 import {getUsersThunk} from '../redux/operations';
 
